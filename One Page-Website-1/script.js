2852ea3b66745937ddf46fea77f2c180021b6561
shivamgarg82 <ngarg82871@gmail.com>
2024-06-09 13:14:29 +0530

    Delete One Page-Website-1/script.js

--- One Page-Website-1/script.js
+++
@@ -1,9 +0,0 @@
-let btn = document.querySelectorAll('.six__b');
-let name = document.getElementById('name');
-let email = document.getElementById('email');
-let message = document.getElementById('message');
-btn.addEventListener('click', () => {
-  let savename = localstorage.setItem('name',name.value);
-  let saveemail = localstorage.setItem('email',email.value);
-  let savemessage = localstorage.setItem('message',message.value);
-});
